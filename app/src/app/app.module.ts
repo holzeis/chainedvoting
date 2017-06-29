7281ffd8f0a5fb7ff8cc95f3d942665e4f9b463f
Richard Holzeis <richard.holzeis@at.ibm.com>
2017-06-29 07:30:09 +0200

    added poll component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PollComponent } from './components/poll/poll.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollComponent
   ],
   imports: [
     BrowserModule,
