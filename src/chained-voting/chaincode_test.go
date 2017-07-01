f08e80c8fac3e9bedf32649667dfd8df438b1f53
borisbuechel <boris.buechel@de.ibm.com>
2017-07-02 01:27:52 +0200

    removed dirty code

--- src/chained-voting/chaincode_test.go
+++ src/chained-voting/chaincode_test.go
@@ -1,74 +1,71 @@
 package main
 
 import (
-	"testing"
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"chained-voting/entities"
+	"chained-voting/util"
 	"encoding/json"
-	"main/entities"
-	"main/util"
 	"errors"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"testing"
 	"time"
 )
 
-var vote1 = entities.Vote {
-	VoteID:				"vote1",
-	Description:		"This is my first vote",
-	Timestamp:			time.Now(),
-	Voter:				"johndoe@us.ibm.com",
-	OptionID:			"like",
-	Voting:				"voting1",
+var vote1 = entities.Vote{
+	VoteID:      "vote1",
+	Description: "This is my first vote",
+	Timestamp:   time.Now(),
+	Voter:       "johndoe@us.ibm.com",
+	OptionID:    "like",
+	Voting:      voting1,
 }
 
-var vote2 = entities.Vote {
-	VoteID:				"vote2",
-	Description:		"This is my second vote which was delegated to me",
-	Timestamp:			time.Now(),
-	Voter:				"mustermann@us.ibm.com",
-	OptionID:			"like",
-	DelegatedVoter:		"johndoe@us.ibm.com"
-	Voting:				"voting1",
+var vote2 = entities.Vote{
+	VoteID:         "vote2",
+	Description:    "This is my second vote which was delegated to me",
+	Timestamp:      time.Now(),
+	Voter:          "mustermann@us.ibm.com",
+	OptionID:       "like",
+	DelegatedVoter: "johndoe@us.ibm.com",
+	Voting:         voting1,
 }
 
-var vote3 = entities.Vote {
-	VoteID:				"vote3",
-	Description:		"This is my first vote which was not delegated from me",
-	Timestamp:			time.Now(),
-	Voter:				"mustermann@us.ibm.com",
-	OptionID:			"like",
-	Voting:				"voting1",
+var vote3 = entities.Vote{
+	VoteID:      "vote3",
+	Description: "This is my first vote which was not delegated from me",
+	Timestamp:   time.Now(),
+	Voter:       "mustermann@us.ibm.com",
+	OptionID:    "like",
+	Voting:      voting1,
 }
 
 var user1 = entities.User{
-	Email:				"johndoe@us.ibm.com",
-	MyVotes:			[]entities.Vote{vote1, vote2},
-	DelegatedVotes:		[]entities.Vote{vote2},
-	MyVotings:			[]entities.Voting{voting1},
-	
+	Email:          "johndoe@us.ibm.com",
+	MyVotes:        []entities.Vote{vote1, vote2},
+	DelegatedVotes: []entities.Vote{vote2},
+	MyVotings:      []entities.Voting{voting1},
 }
 
 var user2 = entities.User{
-	Email:				"mustermann@us.ibm.com",
-	MyVotes:			[]entities.Vote{vote2},
-	DelegatedVotes:		[]entities.Vote{},
-	MyVotings:			[]entities.Voting{},
-	
+	Email:          "mustermann@us.ibm.com",
+	MyVotes:        []entities.Vote{vote2},
+	DelegatedVotes: []entities.Vote{},
+	MyVotings:      []entities.Voting{},
 }
 
 var voting1 = entities.Voting{
-	VotingID:			"voting1",
-	Name:				"The first voting",
-	Description:		"only with one option 'like'",
-	Owner:				"johndoe@us.ibm.com",
-	ValidFrom:			time.Now(),
-	ValidTo:			time.Date(9999, 12, 31, 0, 0, 0, 0, time.UTC),
-	Options:			[]entities.Option {like},
-	Votes:				[]entities.Vote {vote1, vote2},
+	VotingID:    "voting1",
+	Name:        "The first voting",
+	Description: "only with one option 'like'",
+	Owner:       "johndoe@us.ibm.com",
+	ValidFrom:   time.Now(),
+	ValidTo:     time.Date(9999, 12, 31, 0, 0, 0, 0, time.UTC),
+	Options:     []entities.Option{like},
+	Votes:       []entities.Vote{vote1, vote2},
 }
 
 var like = entities.Option{
-	OptionID:			"like",
-	Description:		"The one and only option",
-}
+	OptionID:    "like",
+	Description: "The one and only option",
 
 func Test_WillCreateUserDirectly(t *testing.T) {
 	stub := shim.NewMockStub("ex02", new(Chaincode))
@@ -80,7 +77,6 @@ func Test_WillCreateUserDirectly(t *testing.T) {
 
 }
 
-
 func Test_WillCreateVotingDirectly(t *testing.T) {
 	stub := shim.NewMockStub("ex02", new(Chaincode))
 	stub.MockTransactionStart("CreateVotingDirectlyTx")
@@ -89,7 +85,6 @@ func Test_WillCreateVotingDirectly(t *testing.T) {
 	}
 	stub.MockTransactionEnd("CreateVotingDirectlyTx")
 }
-
 
 func Test_WillCreateVotesDirectly(t *testing.T) {
 	stub := shim.NewMockStub("ex02", new(Chaincode))
@@ -159,8 +154,8 @@ func getVoting(stub *shim.MockStub, votingID string) (entities.Voting, error) {
 	return storedVoting, nil
 }
 
-func getUser(stub *shim.MockStub, userID string) (entities.User, error){
-	key, err := stub.CreateCompositeKey(util.UsersIndexName, []string{user.UserID})
+func getUser(stub *shim.MockStub, email string) (entities.User, error) {
+	key, err := stub.CreateCompositeKey(util.UsersIndexName, []string{user.email})
 	if err != nil {
 		return entities.User{}, err
 	}
@@ -179,8 +174,8 @@ func getUser(stub *shim.MockStub, userID string) (entities.User, error){
 	return storedUser, nil
 }
 
-func createUser(stub *shim.MockStub) (error) {
-	userAsBytes, _ := json.Marshal(user);
+func createUser(stub *shim.MockStub) error {
+	userAsBytes, _ := json.Marshal(user)
 	util.StoreObjectInChain(stub, user.UserID, util.UsersIndexName, userAsBytes)
 
 	storedUser, err := getUser(stub, user.UserID)
@@ -188,14 +183,14 @@ func createUser(stub *shim.MockStub) (error) {
 		return err
 	}
 
-	if (user.UserID != storedUser.UserID) {
+	if user.UserID != storedUser.UserID {
 		return errors.New("Stored ID not the same")
 	}
 	return nil
 }
 
-func createVoting(stub *shim.MockStub) (error) {
-	votingAsBytes, _ := json.Marshal(voting1);
+func createVoting(stub *shim.MockStub) error {
+	votingAsBytes, _ := json.Marshal(voting1)
 	err := util.StoreObjectInChain(stub, voting1.VotingID, util.VotingsIndexName, votingAsBytes)
 	if err != nil {
 		return err
@@ -206,15 +201,14 @@ func createVoting(stub *shim.MockStub) (error) {
 		return err
 	}
 
-	if (voting1.VotingID != storedVoting.VotingID) {
+	if voting1.VotingID != storedVoting.VotingID {
 		return errors.New("Stored ID not the same")
 	}
 	return nil
 }
 
-
-func createVote(stub *shim.MockStub, vote *entities.Vote) (error) {
-	voteAsBytes, _ := json.Marshal(vote);
+func createVote(stub *shim.MockStub, vote *entities.Vote) error {
+	voteAsBytes, _ := json.Marshal(vote)
 	err := util.StoreObjectInChain(stub, vote.VoteID, util.VotesIndexName, voteAsBytes)
 	if err != nil {
 		return err
@@ -225,7 +219,7 @@ func createVote(stub *shim.MockStub, vote *entities.Vote) (error) {
 		return err
 	}
 
-	if (vote.VoteID != storedVote.VoteID) {
+	if vote.VoteID != storedVote.VoteID {
 		return errors.New("Stored ID not the same")
 	}
 	return err
