e3e8be6f3b7d52530d87fee823fc5041b77e376c
Richard Holzeis <richard.holzeis@at.ibm.com>
2017-08-17 10:49:28 +0200

    introduced routing guard

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OnInit } from '@angular/core';
+
+import { AlertService } from './services/alert.service';
+
+
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +13,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Chained Voting';
+  currentUser: User;
+
+  constructor(private alertService: AlertService, private router: Router, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  public logout() {
+    console.log("clicked logout!");
+    localStorage.removeItem('currentUser');
+    this.currentUser = null;
+    //this.router.navigate(['/'])
+  }
 }
