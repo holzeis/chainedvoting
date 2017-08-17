845cf521cb8bf75efd6410258087983bfb7abe61
Richard Holzeis <richard.holzeis@at.ibm.com>
2017-08-17 10:46:21 +0200

    generated login component

---
+++ client/src/app/components/user/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService } from '../../services/user.service';
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public model: any = {};
+  public loading = false;
+  public returnUrl: string;
+
+  constructor(private userService: UserService,  private alertService: AlertService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // reset login status
+    this.userService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+
+  public login() {
+    this.loading = true;
+    this.userService.login(this.model.email).then(res => this.router.navigate([this.returnUrl])).catch(error => this.alertService.error(error));
+  }
+}
